6ecb6c3bf63e0e0dda7756931d8db29cc24d401d
Schossi <reinhardschossleitner@gmail.com>
2022-08-27 10:19:01 +0200

    exits to main scene, some adjustments for build

--- Assets/Scripts/SetupActivator.cs
+++ Assets/Scripts/SetupActivator.cs
@@ -5,6 +5,9 @@ public class SetupActivator : MonoBehaviour
 {
     private void Awake()
     {
+        if (gameObject.scene.name == "ConnectorSouls")
+            return;//startup in build has no Common
+
         if (SceneManager.GetActiveScene() == gameObject.scene)
         {
             SceneManager.MoveGameObjectToScene(gameObject, SceneManager.GetSceneByName("Common"));
